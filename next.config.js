dbc7ceb363cddebe2cc9e85213766a44be0cd479
jawadGitNext <jawadGitNext2021@gmail.com>
2021-06-30 12:00:15 +0300

    final build

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 module.exports = {
   images: {
     domains: [
-      "cdn.pixabay.com",
-      "fakestoreapi.com",
-      "images-na.ssl-images-amazon.com",
-      "techcrunch.com",
+      'cdn.pixabay.com',
+      'fakestoreapi.com',
+      'images-na.ssl-images-amazon.com',
+      'techcrunch.com',
     ],
   },
+  target: 'serverless',
 }
