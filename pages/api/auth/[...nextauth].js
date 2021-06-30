0ac6ea7d4263e2b465940cb53f08bed05491cbcd
jawadGitNext <jawadGitNext2021@gmail.com>
2021-06-30 13:19:42 +0300

    final build

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,9 +1,5 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
-import Adapters from 'next-auth/adapters'
-// import { PrismaClient } from '@prisma/client'
-
-// const prisma = new PrismaClient()
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -14,5 +10,4 @@ export default NextAuth({
       clientSecret: 'xYQ9yPbzfteq3yrh84L6WS5P',
     }),
   ],
-  // adapter: Adapters.Prisma.Adapter({ prisma }),
 })
