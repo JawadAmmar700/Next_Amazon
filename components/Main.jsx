9aff4d03cb44852baaa237c6207ed10a45ccb72f
jawadGitNext <jawadGitNext2021@gmail.com>
2021-09-27 10:26:06 +0300

    new firebase and env

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Carousal from './Carousal'
-import Products from './Product'
+import React from "react"
+import Carousal from "./Carousal"
+import Product from "./Product"
 
 const Main = ({ data, categories }) => {
   return (
     <div className="w-full sm:w-4/5 relative  bg-gray-100   mr-auto ml-auto ">
       <Carousal />
-      <Products data={data} categories={categories} />
+      <Product data={data} categories={categories} />
     </div>
   )
 }
